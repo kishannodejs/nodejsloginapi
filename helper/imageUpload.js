e59bc27a8d6822bc7d2a939bc9c6776caccae86b
kishan <kishan.k@royalcyber.com>
2021-10-14 15:41:43 +0530

    done

--- helper/imageUpload.js
+++ helper/imageUpload.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_USER_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_USER_NAME||'AIzaSyBONcYIVhb_FN1Ui7BcEI6e7iEH3cgNQpc',
+  api_key: process.env.CLOUDINARY_API_KEY||'AIzaSyBONcYIVhb_FN1Ui7BcEI6e7iEH3cgNQpc',
+  api_secret: process.env.CLOUDINARY_API_SECRET||'AIzaSyBONcYIVhb_FN1Ui7BcEI6e7iEH3cgNQpc',
 });
 
 module.exports = cloudinary;
